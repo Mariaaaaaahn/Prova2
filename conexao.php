59e1321f369754d3ca60faff6e1c1df61b1d3fc4
Mariaaaaaahn <pontinm758@gmail.com>
2023-06-20 09:44:27 -0300

    intervalo

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
-
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "prova2";
+$database = "fluxo_caixa";
 $port = 3307;
 $con = mysqli_connect($hostname, $username, $password, $database, $port);
+
 if(mysqli_connect_errno()){
-    printf("Erro conexão: %s\n",
-    mysqli_connect_error());
-    exit();
+    echo "Erro conexão: %s\n",
+        mysqli_connect_error();
+        exit();
 }
-   
+
 ?>
--- prova2.sql
+++ prova2.sql
@@ -1,4 +1,7 @@
-drop table if exists fluxo_caixa;
+DROP DATABASE IF EXISTS fluxo_caixa;
+CREATE DATABASE fluxo_caixa;
+USE fluxo_caixa;
+
 create table fluxo_caixa(
 id  integer NOT NULL AUTO_INCREMENT,
 data DATE NOT NULL, 
@@ -7,4 +10,5 @@ valor DECIMAL(10,2) NOT NULL,
 historico varchar(150) not null, 
 cheque varchar(3) not null, 
 primary key (id));
+
 
